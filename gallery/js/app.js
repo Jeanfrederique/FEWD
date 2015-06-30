e23a3b300a96530bad10151c1ef8c68bcd9c91a7
jean frederique <jfrederique@jibe.com>
2015-06-29 21:15:17 -0700

    Added contact form to gallery

--- gallery/js/app.js
+++ gallery/js/app.js
@@ -4,9 +4,12 @@ sliderElement.Slider();
 var navElement = document.getElementById('nav');
 navElement.Nav();
 
+navElement.showSection('contact');
+
 
 var galleryElemnt = document.getElementById('gallery');
 galleryElemnt.Gallery();
 
-// var singlePhotoElemnt = document.getElementsByClassName('gallery');
-// galleryElemnt.singlePhoto();
+
+var contactElement = document.getElementById('contact');
+contactElement.Contact();
--- gallery/js/nav.js
+++ gallery/js/nav.js
@@ -1,8 +1,13 @@
 Element.prototype.Nav = function(){
 
   var nav = this,
+
+      navItems = nav.children[0].children;
+      sections = document.getElementsByTagName('section');
       btn = document.createElement('div'),
       container = document.getElementById('container');
+
+      console.log(sections.gallery);
 
   this.toggleNav = function(){
     if(container.style.left === "0px"){
@@ -23,9 +28,46 @@ Element.prototype.Nav = function(){
 
   };
 
+
+  //Hide all the sections
+  this.hideSection = function(){
+    for(var i=0; i<sections.length; i++){
+      sections[i].style.opacity = '0';
+      sections[i].style.zIndex = '0';
+      sections[i].style.display = 'none';
+    }
+
+  }
+
+  // Show a section
+  this.showSection = function(id){
+    this.hideSection();
+    //console.log(document.getElementById('id').style)
+    console.log(document.getElementById(id))
+    document.getElementById(id).style.display = 'block';
+    document.getElementById(id).style.opacity = '1.0';
+    document.getElementById(id).style.zIndex = '50';
+  }
+
+
+
+
   this.init = function(){
 
     nav.createButton();
+
+    //loop thru all li and add event listener
+    for(var i=0; i<navItems.length; i++){
+      navItems[i].addEventListener('click', function(evt){
+        
+        var id = evt.target.dataset.section;
+        nav.showSection(id);
+        nav.toggleNav();
+
+        //console.log(id);
+      })
+    };
+
 
   };
 
